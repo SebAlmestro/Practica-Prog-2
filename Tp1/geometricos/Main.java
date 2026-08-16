2799e6b43da0a8a8a0dfd7528566fc7c01a97167
Sebastián Almestro <sebalmestro@gmail.com>
2026-08-16 16:40:28 -0300

    tp1

--- Tp1/geometricos/Main.java
+++ Tp1/geometricos/Main.java
@@ -1,5 +1,11 @@
 package geometricos;
 
 public class Main {
-    
+    public static void main(String[] args) {
+        PuntoGeometrico pg = new PuntoGeometrico(1, 1);
+        System.out.println(pg);
+        pg.desplazarPuntoGeometrico(pg.getX(), pg.getY(), 2, 2);
+        System.out.println(pg);
+        
+    }
 }
--- Tp1/geometricos/PuntoGeometrico.java
+++ Tp1/geometricos/PuntoGeometrico.java
@@ -1,5 +1,59 @@
 package geometricos;
 
 public class PuntoGeometrico {
-    
+    //defecto y const globales
+    private final static int defecY = 0;
+    private final static int defecX = 0;
+
+    //atributos
+    private int X;
+    private int Y;
+
+    //contructores
+    public PuntoGeometrico(){
+        this.X = defecX;
+        this.Y = defecY;
+    }
+
+    public PuntoGeometrico(int X, int Y){
+        this.X = X;
+        this.Y = Y;
+    }
+
+    //responsabilidades
+    public void desplazarPuntoGeometrico(int X, int Y, int desX, int desY){
+     this.X += (desX);
+    this.Y += (desY);
+        
+    }
+
+    public int getX() {
+        return X;
+    }
+
+    public void setX(int x) {
+        X = x;
+    }
+
+    public int getY() {
+        return Y;
+    }
+
+    public void setY(int y) {
+        Y = y;
+    }
+
+    // toString
+    @Override
+    public String toString() {
+        return "Electrodomestico{" +
+                "X= " + X + '\'' +
+                ", Y= " + Y +
+                '}';
+    }
+
+
+
+
+
 }
